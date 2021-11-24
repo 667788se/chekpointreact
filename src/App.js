a4b0fec7854b8ff366acb14b103d996802127b88
Your Name <sehamerhoma@gmail.com>
2021-11-24 16:07:50 +0200

    Adding chekpoint

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button,Navbar,Container,NavDropdown,Nav } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <Button >Button #1</Button>
+
+     <Navbar bg="light">
+    <Container>
+      <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="light">
+    <Container>
+      <Navbar.Brand>Brand text</Navbar.Brand>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="dark">
+  <Container>
+    <Navbar.Brand href="#home">
+      <img
+        src="https://react-bootstrap.netlify.app/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+  </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      React Bootstrap
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+     
+
+  <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+    
+
+
+<Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+  <br />
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
